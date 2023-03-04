e8a287824369840bf7275ddb0affe5876602e28d
Craig McCoy <craig.graham.mccoy@gmail.com>
2023-03-04 16:34:28 -0700

    Major refactor of code...break out functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,190 +1,78 @@
 #include <Arduino.h>
-#include <FastLED.h>
-#include <NewPing.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <util/atomic.h>
+
+#include "sonic.h"
+#include "sensor.h"
+#include "display.h"
+#include "pixels.h"
 
 // defining the pins
-#define DHT_PIN 2     // D0
-#define LED_PIN 7     // D4
-#define TRIGGER_PIN 9 // D6
-#define ECHO_PIN 10   // D7
-// #define LCD_DATA_PIN 23   // SDA/A4/D18
-// #define LCD_CLOCK_PIN 24  // SCL/A5/D19
+#define HEARTBEAT_LED_PIN LED_BUILTIN // D10 (Pin 13)
 
-#define NUM_LEDS 15 // The number of LEDs
-#define OFF CRGB(0, 0, 0)
-#define GREEN CRGB(0, 255, 0)
-#define RED CRGB(255, 0, 0)
-
-#define MAX_DISTANCE 400                             // Maximum distance we want to measure (in centimeters).
-#define NUMBER_OF_SAMPLES 15                         // the number of samples to take
-#define SPEED_OF_SOUND 0.0343                        // in cm per microseconds
-#define DISTANCE(time) ((time * SPEED_OF_SOUND) / 2) // Divide by 2 because we are measuring the duration the sound wave needs to travel to the object and bounce back
-
-#define LCD_ROWS 2
-#define LCD_COLUMNS 16
-
-#define DHTTYPE DHT22 // DHT 22  (AM2302)
+#define CALCULATE_SPEED_OF_SOUND_INTERVAL 60 // One Minute (60 Seconds)
 
 // defining variables
-CRGB leds[NUM_LEDS];
-unsigned long startdistance = MAX_DISTANCE; // distance from sensor to begin scan as car pulls in(CENTIMETERS)
-unsigned long stopdistance = 115;           // parking position from sensor (CENTIMETERS)
-unsigned long incrementdistance = ((startdistance - stopdistance) / NUM_LEDS);
-unsigned long delayMS;
+volatile float speedOfSound;
 
-void setLedColors(int lastLed, CRGB color);
-
-// create an instance of the ultrasonic distance sensor
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-// create an instance of the temperature/humidity sensor
-DHT_Unified dht(DHT_PIN, DHTTYPE);
-// create an instance of LCD display: address = 0x27 and configure for a 16 character and 2 line display
-LiquidCrystal_I2C lcd(0x27, LCD_COLUMNS, LCD_ROWS);
+// Forward referenced functions
+void InitializeOneSecondTimerInterrupt();
 
 void setup()
 {
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS); // set up the LED strip
-  dht.begin();                                           // start the temperature/humidity sensor
-  lcd.init();                                            // initialize the lcd
-  lcd.clear();                                           // clear the lcd display
-  lcd.backlight();                                       // open the backlight
-  Serial.begin(9600);                                    // start the serial communication
-
-  // Initialize temperature/humidity device.
-  Serial.println(F("DHT22 Unified Sensor Example"));
-  // Print temperature sensor details.
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("%"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("%"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
+  Serial.begin(9600);                 // start the serial communication
+  pinMode(HEARTBEAT_LED_PIN, OUTPUT); // set up the Heartbeat LED
+  InitializeTemperatureAndHumidityDevice();
+  speedOfSound = CalculateSpeedOfSound(); // Calculate the initial speedOfSound
+  InitializePixelLeds(MAX_DISTANCE, 115);
+  InitializeDisplay();
+  InitializeOneSecondTimerInterrupt();
 }
 
 void loop()
 {
-  // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
-  sensors_event_t event;
-  int temperature, humidity;
-  float speedOfSound, distance, duration;
-
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature))
+  float sos;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
   {
-    Serial.println(F("Error reading temperature!"));
+    sos = speedOfSound;
   }
-  else
-  {
-    temperature = event.temperature;
-    Serial.print(F("Temperature: "));
-    Serial.print(temperature);
-    Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity))
-  {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else
-  {
-    humidity = event.relative_humidity;
-    Serial.print(F("Humidity: "));
-    Serial.print(humidity);
-    Serial.println(F("%"));
-  }
-  speedOfSound = 331.4 + (0.6 * temperature) + (0.0124 * humidity); // Calculate speed of sound in m/s
-
-  duration = sonar.ping_median(NUMBER_OF_SAMPLES); // returns duration in microseconds
-  duration /= 1000000;                             // Convert microseconds to seconds
-  distance = (speedOfSound * duration) / 2;        // Divide by 2 because we are measuring the duration the sound wave needs to travel to the object and bounce back
-  distance = distance * 100;                       // meters to centimeters
-
+  unsigned long distance = CalculateSonicDistance(sos);
   // Prints the distance on the Display
-  lcd.setCursor(0, 0);     // Sets the location at which subsequent text written to the LCD will be displayed
-  lcd.print("Distance: "); // Prints string "Distance" on the LCD
-  lcd.print(distance);     // Prints the distance value from the sensor
-  lcd.print(" cm");
-  // Prints the distance on the Serial Monitor
-  Serial.print(F("Distance: "));
-  Serial.print(distance);
-  Serial.println(F("cm"));
+  PrintfLine(2, PSTR("Distance: %3dcm"), distance);
 
-  if (distance >= startdistance)
-  {
-    // Greater than start distance -- set all LEDS off
-    setLedColors(NUM_LEDS, OFF);
-  }
-  if (distance <= stopdistance)
-  {
-    // Less than or equal to the stop distance -- set all LEDS to red
-    setLedColors(NUM_LEDS, RED);
-  }
-  else
-  {
-    for (int i = 1; i <= NUM_LEDS; i++)
-    {
-      if (distance <= stopdistance + (incrementdistance * i))
-      {
-        setLedColors(i, GREEN);
-        break;
-      }
-    }
-  }
+  SetPixelLeds(distance);
 }
 
-void setLedColors(int lastLed, CRGB color)
+void InitializeOneSecondTimerInterrupt()
 {
-  for (int i = 0; i < lastLed; i++)
+  cli();                               // Disable interrupts while setting up registers
+  TCCR1A = 0;                          // Reset entire TCCR1A to 0
+  TCCR1B = 0;                          // Reset entire TCCR1B to 0
+  TCNT1 = 0;                           // initialize counter value to 0
+  TCCR1B |= (1 << WGM12);              // turn on CTC (Clear timer on compare match) mode
+  TCCR1B |= (1 << CS12) | (1 << CS10); // Set CS10 and CS12 bits for 1024 prescaler
+  TIMSK1 |= (1 << OCIE1A);             // Enable compare match mode
+  // Set Compare Match Register = clock frequency / prescaler / interrupt frequency - 1 (cause its zero based)
+  OCR1A = 15624; // 16MHz / 1024 / 1Hz - 1 (must be <65536)
+  sei();         // Enable back the interrupts
+}
+
+// One Second Timer Interrupt Service Routine (ISR)
+ISR(TIMER1_COMPA_vect)
+{
+  // Blink heartbeat LED every interrupt
+  static bool heartBeatState = true;
+  static int calculateSpeedOfSoundCounter = CALCULATE_SPEED_OF_SOUND_INTERVAL;
+
+  heartBeatState = !heartBeatState;                // Invert LED state
+  digitalWrite(HEARTBEAT_LED_PIN, heartBeatState); // Write new state to the LED
+
+  if (--calculateSpeedOfSoundCounter <= 0)
   {
-    leds[i] = color;
+    float sos = CalculateSpeedOfSound();
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+      speedOfSound = sos;
+    }
+    calculateSpeedOfSoundCounter = CALCULATE_SPEED_OF_SOUND_INTERVAL;
   }
-  for (int i = lastLed; i < NUM_LEDS; i++)
-  {
-    leds[i] = OFF;
-  }
-  FastLED.show();
-  delay(50);
 }
