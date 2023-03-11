3896db558e91ec172fb541ef51a0bb3f3c486b91
Craig McCoy <craig.graham.mccoy@gmail.com>
2023-03-11 16:43:11 -0700

    Modifications to the timing of events.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@
 // defining the pins
 #define HEARTBEAT_LED_PIN LED_BUILTIN // D10 (Pin 13)
 
-#define CALCULATE_SPEED_OF_SOUND_INTERVAL 60 // One Minute (60 Seconds)
+#define CALCULATE_HEARTBEAT_INTERVAL       1 * 4 // Second (1 Second * 4Hz)
+#define CALCULATE_SPEED_OF_SOUND_INTERVAL 60 * 4 // One Minute (60 Seconds * 4Hz)
 
 #define CELSIUS_TO_FAHRENHEIT(temperature) (int)(((double)temperature * 9.0 / 5.0) + 32.5)
 
@@ -19,6 +20,7 @@
 static int temperature;
 static int humidity;
 volatile bool updateSpeedOfSound = true;
+volatile bool updateDistances = true;
 float _speedOfSound;
 // The Stored Data Manager (EEPROM Settings)
 StoredDataManager *_storedDataManager = nullptr;
@@ -81,35 +83,38 @@ void setup()
 
 void loop()
 {
-  float speedOfSound;
+  static float speedOfSound;
   if (updateSpeedOfSound)
   {
     updateSpeedOfSound = false;
     speedOfSound = CalculateSpeedOfSound(&temperature, &humidity);
     DisplayTemperatureAndHumidity(temperature, humidity);
   }
-  delay(200);
-  unsigned long currentDistance[NUMBER_OF_SONARS];
-  unsigned long startDistance[NUMBER_OF_SONARS];
-  unsigned long stopDistance[NUMBER_OF_SONARS];
-  for (int index = 0; index < NUMBER_OF_SONARS; index++)
+  if (updateDistances)
   {
-    currentDistance[index] = CalculateSonicDistance(index, speedOfSound);
-    Serial.print("Current Distance");
-    Serial.print(index);
-    Serial.print(": ");
-    Serial.print(currentDistance[index]);
-    Serial.println(F("cm"));
+    updateDistances = false;
+    unsigned long currentDistance[NUMBER_OF_SONARS];
+    unsigned long startDistance[NUMBER_OF_SONARS];
+    unsigned long stopDistance[NUMBER_OF_SONARS];
+    for (int index = 0; index < NUMBER_OF_SONARS; index++)
+    {
+      currentDistance[index] = CalculateSonicDistance(index, speedOfSound);
+      Serial.print(F("Current Distance"));
+      Serial.print(index);
+      Serial.print(F(": "));
+      Serial.print(currentDistance[index]);
+      Serial.println(F("cm"));
 
-    SetCurrentDistance(index, currentDistance[index]);
-    startDistance[index] = _storedDataManager->getStartDistance(index);
-    stopDistance[index] = _storedDataManager->getStopDistance(index);
+      SetCurrentDistance(index, currentDistance[index]);
+      startDistance[index] = _storedDataManager->getStartDistance(index);
+      stopDistance[index] = _storedDataManager->getStopDistance(index);
+    }
+    //  Prints the distance on the Display
+    DisplayCurrentDistance(currentDistance);
+    DisplayStartDistance(startDistance);
+    DisplayStopDistance(stopDistance);
+    SetPixelLeds(currentDistance, startDistance, stopDistance);
   }
-  //  Prints the distance on the Display
-  DisplayCurrentDistance(currentDistance);
-  DisplayStartDistance(startDistance);
-  DisplayStopDistance(stopDistance);
-  SetPixelLeds(currentDistance, startDistance, stopDistance);
 }
 
 void InitializeOneSecondTimerInterrupt()
@@ -123,7 +128,7 @@ void InitializeOneSecondTimerInterrupt()
   TCCR1B |= (1 << CS12) | (1 << CS10); // Set CS10 and CS12 bits for 1024 prescaler
   TIMSK1 |= (1 << OCIE1A);             // Enable compare match mode
   // Set Compare Match Register = clock frequency / prescaler / interrupt frequency - 1 (cause its zero based)
-  OCR1A = 15624; // 16MHz / 1024 / 1Hz - 1 (must be <65536)
+  OCR1A = 3906; // 16MHz / 1024 / 4Hz - 1 (must be <65536)
   sei();         // Enable back the interrupts
 }
 
@@ -132,14 +137,20 @@ ISR(TIMER1_COMPA_vect)
 {
   // Blink heartbeat LED every interrupt
   static bool heartBeatState = true;
+  static int heartbeatCounter = CALCULATE_HEARTBEAT_INTERVAL;
   static int calculateSpeedOfSoundCounter = CALCULATE_SPEED_OF_SOUND_INTERVAL;
 
-  heartBeatState = !heartBeatState;                // Invert LED state
-  digitalWrite(HEARTBEAT_LED_PIN, heartBeatState); // Write new state to the LED
+  if (--heartbeatCounter <= 0)
+  {
+    heartBeatState = !heartBeatState;                // Invert LED state
+    digitalWrite(HEARTBEAT_LED_PIN, heartBeatState); // Write new state to the LED
+    heartbeatCounter = CALCULATE_HEARTBEAT_INTERVAL;
+  }
 
   if (--calculateSpeedOfSoundCounter <= 0)
   {
     updateSpeedOfSound = true;
     calculateSpeedOfSoundCounter = CALCULATE_SPEED_OF_SOUND_INTERVAL;
   }
+  updateDistances = true;
 }
