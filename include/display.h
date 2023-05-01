2264eff750279d363ed3fcbf54ff0b6f571e7dfd
Craig McCoy <craig.graham.mccoy@gmail.com>
2023-05-01 16:00:56 -0600

    Add error message for OLED display

--- include/display.h
+++ include/display.h
@@ -1,9 +1,22 @@
 #pragma once
 
+// Screen dimensions
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
+#define FONT_WIDTH 6
+#define FONT_HEIGHT 8
+#define FONT_SCALE 1
+#define LCD_ROWS (int)(SCREEN_HEIGHT / FONT_HEIGHT) 
+#define LCD_COLUMNS (int)(SCREEN_WIDTH / FONT_WIDTH)
+
 #define ROW1 0
 #define ROW2 1 
 #define ROW3 2
 #define ROW4 3
+
+#define ERROR_ROW1 (int)(LCD_ROWS - 3) 
+#define ERROR_ROW2 (int)(LCD_ROWS - 2) 
+#define ERROR_ROW3 (int)(LCD_ROWS - 1) 
 
 // Color definitions
 #define BLACK 0x0000
--- src/display.cpp
+++ src/display.cpp
@@ -7,15 +7,6 @@
 
 #include "display.h"
 
-// Screen dimensions
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
-#define FONT_WIDTH 6
-#define FONT_HEIGHT 8
-#define FONT_SCALE 1
-#define LCD_ROWS (int)(SCREEN_HEIGHT / FONT_HEIGHT)
-#define LCD_COLUMNS (int)(SCREEN_WIDTH / FONT_WIDTH)
-
 // These are the h/w pins for the OLED Display (for fastest operation)
 #define SCLK_PIN 13 // D13
 #define MOSI_PIN 11 // D11
@@ -31,7 +22,7 @@ char _line[LCD_ROWS][LCD_COLUMNS + 1];
 char _line_copy[LCD_ROWS][LCD_COLUMNS];
 
 void display();
-void updateDisplayRow(int row);
+void updateDisplayRow(int row, uint16_t color, uint16_t bg);
 
 void InitializeDisplay()
 {
@@ -49,7 +40,9 @@ void PrintLine(int row, const char *line)
     }
     else
     {
-        //Serial.println(F("PrintLine() Error: row out of range!"));
+        PrintLine(ERROR_ROW1, "Error: PrintLine()");
+        PrintfLine(ERROR_ROW2, PSTR("Row %d out of range!"), row);
+        PrintLine(ERROR_ROW3, "");
     }
 }
 
@@ -65,35 +58,25 @@ void PrintfLine(int row, const char *format, ...)
     }
     else
     {
-        //Serial.println(F("PrintfLine() Error: row out of range!"));
-    }
-}
-
-void PrintfLine(int row, uint16_t color, uint16_t bg, const char *format, ...)
-{
-    if (row < LCD_ROWS)
-    {
-        va_list args;
-        va_start(args, format);
-        vsnprintf_P(_line[row], LCD_COLUMNS + 1, format, args);
-        va_end(args);
-        display();
-    }
-    else
-    {
-        //Serial.println(F("PrintfLine() Error: row out of range!"));
+        PrintLine(ERROR_ROW1, "Error: PrintfLine()");
+        PrintfLine(ERROR_ROW2, PSTR("Row %d out of range!"), row);
+        PrintLine(ERROR_ROW3, "");
     }
 }
 
 void display()
 {
-    for (int row = 0; row < LCD_ROWS; row++)
+    for (int row = 0; row < ERROR_ROW1; row++)
     {
-        updateDisplayRow(row);
+        updateDisplayRow(row, WHITE, BLACK);
+    }
+    for (int row = ERROR_ROW1; row < ERROR_ROW3; row++)
+    {
+        updateDisplayRow(row, RED, BLACK);
     }
 }
 
-void updateDisplayRow(int row)
+void updateDisplayRow(int row, uint16_t color, uint16_t bg)
 {
     bool isEnd = false;
     for (int column = 0; column < LCD_COLUMNS; column++)
@@ -109,7 +92,7 @@ void updateDisplayRow(int row)
             _line_copy[row][column] = displayChar;
             int x = column * FONT_WIDTH;
             int y = (row * FONT_HEIGHT) + (FONT_HEIGHT - 1);
-            tft.drawChar(x, y, displayChar, WHITE, BLACK, FONT_SCALE);
+            tft.drawChar(x, y, displayChar, color, bg, FONT_SCALE);
         }
     }
 }
